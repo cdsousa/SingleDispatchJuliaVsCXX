aa45daf8851ab35a5a885bd0da9cd45562c047ec
Cristóvão D. Sousa <crisjss@gmail.com>
2017-07-03 14:19:27 +0100

    Evolve test

--- subclassdispatchtests.cpp
+++ subclassdispatchtests.cpp
@@ -23,35 +23,42 @@ struct ConcrType2: public AbstrType{
 };
 
 int main(int argc, char **argv) {
-    int n = 100000;
+    int n = 100'000;
 
     std::vector<ConcrType1> arrconcr;
     for(int i=0; i<n; i++){
-        arrconcr.push_back(ConcrType1(i));
+        arrconcr.push_back(ConcrType1(i+1));
     }
 
     std::vector<AbstrType*> arrabstr;
     for(int i=0; i<n; i++){
         // alternate object type to inhibit branch prediction (?)
         if(rand()%2){
-            arrabstr.push_back(new ConcrType1(i));
+            arrabstr.push_back(new ConcrType1(i+1));
         }else{
-            arrabstr.push_back(new ConcrType2(i));
+            arrabstr.push_back(new ConcrType2(i+1));
         }
     }
 
 
+    int sumconcr = 0;
     auto t0 = Clock::now();
-    for(auto& e : arrconcr) { e.f(); }
+    for(auto& e : arrconcr) { sumconcr += e.f(); }
     auto t1 = Clock::now();
 
+    int sumabstr = 0;
     auto t2 = Clock::now();
-    for(auto& p : arrabstr) { p->f(); }
+    for(auto& p : arrabstr) { sumabstr += p->f(); }
     auto t3 = Clock::now();
 
 
     std::cout << std::chrono::duration_cast<microseconds>(t1-t0).count() * 1e-6 << " seconds" << std::endl;
     std::cout << std::chrono::duration_cast<microseconds>(t3-t2).count() * 1e-6 << " seconds" << std::endl;
+
+    if(sumconcr != sumabstr){
+        throw std::runtime_error("sumconcr != sumabstr");
+    }
+    std::cout << "(sum=" << sumabstr << ")" << std::endl;
 
     for(auto p: arrabstr) { delete p; }
 
--- subclassdispatchtests.jl
+++ subclassdispatchtests.jl
@@ -1,43 +1,61 @@
-abstract AbstrType
+abstract type AbstrType end
 
-type ConcrType1 <: AbstrType
+struct ConcrType1 <: AbstrType
     x::Int
 end
 f(a::ConcrType1) = a.x
 
-type ConcrType2 <: AbstrType
+struct ConcrType2 <: AbstrType
     x::Int
 end
 f(a::ConcrType2) = a.x
 
+const n = 100_000
+
+const arrconcr = [ConcrType1(i) for i=1:n]
+const arrabstr = AbstrType[rand(Bool) ? ConcrType1(i) : ConcrType2(i) for i=1:n]
+const arrunion = Union{ConcrType1, ConcrType2}[rand(Bool) ? ConcrType1(i) : ConcrType2(i) for i=1:n]
+
+g_arrconcr(i) = arrconcr[i]
+g_arrabstr(i) = arrabstr[i]
+g_arrunion(i) = arrunion[i]
 
 function main()
     println()
     gc()
 
-    n = 100_000
+    sum_arrconcr = 0::Int
+    @time for i=1:n
+        @inbounds sum_arrconcr += f(g_arrconcr(i))
+    end
 
-    arrconcr = [ConcrType1(i) for i=1:n]
-    arrabstr = AbstrType[rand(Bool) ? ConcrType1(i) : ConcrType2(i) for i=1:n]
+    sum_arrabstr = 0::Int
+    @time for i=1:n
+        @inbounds sum_arrabstr += f(g_arrabstr(i))
+    end
 
-
-    @time for a in arrconcr; f(a); end
-
-    @time for a in arrabstr; f(a); end
-
-
-    # Yet another experiment
-    @time for a in arrabstr
+    # manual dispatch
+    sum_arrabstr2 = 0::Int
+    @time for i=1:n
+        @inbounds a = g_arrabstr(i)
         T = typeof(a)
         if T === ConcrType1
-            f(a::ConcrType1)
+            sum_arrabstr2 += f(a::ConcrType1)
         elseif T === ConcrType2
-            f(a::ConcrType2)
+            sum_arrabstr2 += f(a::ConcrType2)
         else
-            f(a)
+            sum_arrabstr2 += f(a)
         end
     end
 
+    sum_arrunion = 0::Int
+    @time for i=1:n
+        @inbounds sum_arrunion += f(g_arrunion(i))
+    end
+
+    @assert sum_arrconcr == sum_arrabstr == sum_arrabstr2 == sum_arrunion
+
+    println("(sum=$sum_arrconcr)")
 end
 
 main()
